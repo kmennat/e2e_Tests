94b7d17a13add205f228d0322dd43e78fc822101
kmennat <khattab.mennat@googlemail.com>
2026-03-29 20:44:59 +0200

    my jenkins file....

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,12 +40,14 @@ export default defineConfig({
   // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [
-    ['allure-playwright'],
-    ['list'],
-    ['html'],
+  // reporter: [
+  //   ['allure-playwright'],
+  //   ['list'],
+  //   ['html'],
+    reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+});
     //['@reportportal/agent-js-playwright', RPconfig]
-  ],
+  //],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
